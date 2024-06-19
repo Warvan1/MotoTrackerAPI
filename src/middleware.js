f67cc7289fad2f4563d037a8ac8467906cec444e
Warvan1 <collinsn@clarkson.edu>
2024-06-18 23:24:01 -0400

    added maintenance log recycler view

--- src/middleware.js
+++ src/middleware.js
@@ -58,4 +58,23 @@ const requireCarWithAccess = async (req, res, next) => {
 
 }
 
-module.exports = { requireUserIDHeader, requireUser, requireCarIDQuery, requireCarWithAccess }
+const ifCarCheckAccess = async (req, res, next) => {
+    if(req.query.car_id != null && req.headers.user_id != null){
+        //get the car from the database
+        var car = await db.query("select * from cars where car_id = $1 and user_id = $2;", [req.query.car_id, req.headers.user_id]);
+        //if the user object is not found return
+        if(car.rows.length != 1){
+            res.json(null);
+            return;
+        }
+        //add the car object to the req object
+        req.car_db = car.rows[0]
+        next();
+    }
+    else{
+        next();
+    }
+
+}
+
+module.exports = { requireUserIDHeader, requireUser, requireCarIDQuery, requireCarWithAccess, ifCarCheckAccess }
--- src/routes/maintenanceLog.js
+++ src/routes/maintenanceLog.js
@@ -1,25 +1,38 @@
-
 var router = require('express').Router();
 var jsonParser = require('body-parser').json();
 const { jwtCheck, db } = require('../utils.js');
-const { requireCarWithAccess } = require('../middleware.js');
+const { requireCarWithAccess, ifCarCheckAccess, requireUser } = require('../middleware.js');
 
 router.post('/addmaintenance', jwtCheck, jsonParser, requireCarWithAccess, async (req, res) => {
     //TODO: add post body input validation
 
     //add an entry to the maintenance log table
-    await db.query("insert into maintenance(user_id, car_id, service_type, miles, cost, notes) values($1, $2, $3, $4, $5, $6);", 
+    var log = await db.query("insert into maintenance(user_id, car_id, service_type, miles, cost, notes) values($1, $2, $3, $4, $5, $6) returning *;", 
         [req.headers.user_id, req.query.car_id, req.body.type, req.body.miles, req.body.cost, req.body.notes]
     );
 
     //update the cars miles and total_costs
-    await db.query("update cars set miles = $2, total_costs = $3 where car_id = $1", 
+    await db.query("update cars set miles = $2, total_costs = $3 where car_id = $1;", 
         [req.query.car_id, req.body.miles, req.car_db.total_costs + req.body.cost]
     );
 
     //TODO: add querys to update msl and tsl values
 
-    res.json(null);
+    res.json(log.rows[0]);
 });
+
+router.get('/getmaintenancelog', jwtCheck, requireUser, ifCarCheckAccess, async (req, res) => {
+    var car_id;
+    //if the car query is set
+    if(req.car_db != null){
+        car_id = req.car_db.car_id;
+    }
+    else{
+        car_id = req.user_db.current_car;
+    }
+    var log = await db.query("select * from maintenance where car_id = $1", [car_id]);
+
+    res.json(log.rows);
+})
 
 module.exports = router;
